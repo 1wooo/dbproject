e1c7313be1283aba940af248ed93173afcbc830b
1wooo <dnjsdn2468@naver.com>
2022-06-05 18:29:18 +0900

    ver0603

--- login.java
+++ login.java
@@ -5,16 +5,33 @@ import java.awt.event.ActionListener;
 import java.util.*;
 import javax.swing.*;
 import javax.swing.event.*;
+
+
 import java.sql.*;
 
 
 public class login extends JFrame{
+	// DB 객체들
+	DB db_forlogin;
+	ResultSet rs = null;
+	//ResultSetMetaData rsMeta = null;
+	Statement stmt = null;
+	// DB 객체들
 	
+	
+	// 스윙 컴포넌트들
 	JPanel loginbox = new JPanel();
 	JTextField id = new JTextField();
 	JButton chk = new JButton("확인");
+	// 스윙 컴포넌트들 
 	
-	public login() {
+	int chkk = 0;
+	
+	public login(DB db) {
+		db_forlogin = db;
+		rs = db_forlogin.res;
+		stmt = db_forlogin.stmt;
+		
 		setTitle("회원아이디 입력");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
@@ -41,16 +58,45 @@ public class login extends JFrame{
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
+			JOptionPane warning = new JOptionPane();
+			
+			String SQL = "select custId from Customer;";
 			JButton b = (JButton)e.getSource();
-			String st = b.getText();
+			String text = id.getText(); // 텍스트 박스 안에 있는 id 문자열 받아오기
+			String st = b.getText(); // 버튼 텍스트 받아오기
 			
 			if (st == "확인") {
+
+				try {
+					rs = stmt.executeQuery(SQL);
+					
+				} catch (SQLException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} // resulset에 쿼리 실행 결과문 삽입
+
+				try {
+
+					while(rs.next()) {
+						if (text.equals(rs.getString(1))) {
+							chkk = 1;
+							dispose();
+							//System.out.println(chkk);
+							Movie.mv.BG.setVisible(false);
+							Movie.mv.setContentPane(Movie.mv.Mainsc);
+						}
+					}
+
+				}
+				 catch (SQLException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				if (chkk == 0) {
+					warning.showMessageDialog(null,text);	
+				}
 				
-		//// id 텍스트필드에 있는 문자열 받아서 Customer 테이블이랑 비교 ////
-				
-				dispose();
 			}
 		}
-		
 	}
 }
